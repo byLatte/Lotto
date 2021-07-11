58874785ee77e29ae8739a384b6d785fd76cd9e6
dev21 <dev21>
2021-07-11 23:53:25 +0900

    기본틀 구성

--- app/src/main/java/com/latte/lotto/MainActivity.kt
+++ app/src/main/java/com/latte/lotto/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.latte.lotto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.latte.lotto.databinding.ActivityMainBinding
+import com.latte.lotto.extract.ExtractActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = DataBindingUtil.setContentView(this , R.layout.activity_main)
+
+        //번호 뽑기 화면이동
+        mainBinding.extractActivityButton.setOnClickListener {
+            val intent = Intent(applicationContext,ExtractActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/latte/lotto/extract/ExtractActivity.kt
@@ -0,0 +1,12 @@
+package com.latte.lotto.extract
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.latte.lotto.R
+
+class ExtractActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_extract)
+    }
+}
