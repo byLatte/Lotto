845ed769dde77d6792221b7937981934615104a6
dev21 <dev21>
2021-07-19 22:00:29 +0900

    LottoIntentApplication 추가

---
+++ app/src/main/java/com/latte/lotto/LottoIntentApplication.kt
@@ -0,0 +1,15 @@
+package com.latte.lotto
+
+import android.app.Application
+import com.latte.lotto.database.NumberHistoryRepository
+
+//서브 클래스 manifest에도 지정.
+//최초 실행될때 메모리에 로드되며 딱 한번 실행된다.
+//즉 DB 인스턴스를 한번만 생성한다.
+class LottoIntentApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        NumberHistoryRepository.init(this)
+    }
+}
